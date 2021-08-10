64ee1614ab3f48dee472005751b26560fd2b70e3
dengchungui <dengchungui@dcg.com>
2021-08-10 19:03:08 +0800

    second dev branch commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, This is the Git test");
+
+        System.out.println("If you see this, you win");
     }
 }
